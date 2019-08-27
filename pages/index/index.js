c1da986e1958b858e96c123369cc193b072daaaa
刘锐斌 <liuruibin@zhuotaiju.com>
2019-08-27 15:52:31 +0800

    知识点10：事件的介绍、事件的简单演练、常见事件类型、事件类型演练、事件对象介绍、touches和changedTouches的区别、currentTarget和target的区别、事件参数的传递、参数传递的练习、事件冒泡和事件捕获

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    titles: ['衣服', '裤子', '鞋子']
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +16,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -50,5 +51,66 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+
+  handleBtnClick() {
+    console.log('按钮发生点击')
+  },
+  handleTouchStart() {
+    console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
+  },
+  hadleEventClick(event) {
+    console.log('---------------', event)
+  },
+  handleEventEnd(event) {
+    console.log('++++++++++++++', event)
+  },
+  handleInner(event) {
+    console.log('内层view', event)
+  },
+  handleOuter(event) {
+    console.log('外层view', event)
+  },
+  handleItemClick(event) {
+    console.log(event)
+    //  title - index
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title, index)
+
+  },
+
+  // 事件冒泡和事件捕获
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   }
 })
