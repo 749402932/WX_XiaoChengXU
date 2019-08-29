753edf6ae6b7f87851aa259665bef7f36a8b9bb7
刘锐斌 <liuruibin@zhuotaiju.com>
2019-08-29 10:24:07 +0800

    知识点13：页面直接调用自定义组件修改数据/方法、自定义组件插槽（单插槽和多插槽的使用）、Component构造器

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isShow: true
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +16,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -49,6 +50,28 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  handleIncrementCpn() {
+
+    //  最终目的： 修改my-sel中的counter
+    //  1.获取组件对象
+    // const my_sel = this.selectComponent('.sel-class')
+    const my_sel = this.selectComponent('#sel-id') //推荐
+    console.log("------------------", my_sel)
+
+    //  2.通过setData修改组件中的数据（不合理）
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 20
+    // })
+
+    //  3.通过方法对数据进行修改
+    my_sel.incrementCounter(10)
+  },
+
+  handleChangeShow() {
+    this.setData({
+      isShow: !this.data.isShow
     })
   }
 })
