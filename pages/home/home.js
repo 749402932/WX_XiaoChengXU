b7a47193577a0e2e57140e26433f2eb5459ba38a
刘锐斌 <liuruibin@zhuotaiju.com>
2019-08-30 18:02:36 +0800

    轮播图

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,29 +1,40 @@
 // pages/home/home.js
+import {
+  getMultiData
+} from '../../service/home.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    banners: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567168558123&di=05577c9d46068f10c587dae50f9f5719&imgtype=0&src=http%3A%2F%2Fpic33.nipic.com%2F20130924%2F9822353_015119969000_2.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567168575874&di=506038d186ec89325d4e5780136d96cc&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20110718%2FImg313696301.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567168613728&di=79e114ee62f35c5492b1ddf976a57d02&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F5f56362ebfd186e7a8c16b1a00442938cf7ddc9747ba8-nrIZSr_fw658'
+    ],
+    recommends: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    wx.request({
-      url: 'http://123.207.32.32:8000/home/multidata',
-      data: '',
-      header: {},
-      method: 'GET',
-      dataType: 'json',
-      responseType: 'text',
-      success: function(res) {
-        console.log(res)
-      },
-      fail: function(res) {},
-      complete: function(res) {},
+    //  1.请求轮播图以及推荐数据
+    getMultiData().then(res => {
+      console.log('轮播图请求成功', res)
+      const banners = res.data.data.list;
+      const recommends = res.data.recommend.list;
+
+      //  将banners和recommends放到data中
+      // this.setData({
+      //   banners,
+      //   recommends
+      // })
+
+    }).catch(err => {
+      console.log('轮播图请求失败', err)
     })
   },
 
